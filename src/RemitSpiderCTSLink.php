e702e1d3355dc9d3b5358a79de2a7b65bb74af5a
Michael Drennen <michaeldrennen74@gmail.com>
2022-12-19 11:58:30 -0700

    Test.

---
+++ src/RemitSpiderCTSLink.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace DPRMC\RemitSpiderCTSLink;
+
+
+use DPRMC\RemitSpiderUSBank\Collectors\CTSLinkBrowser;
+use HeadlessChromium\Cookies\CookiesCollection;
+use HeadlessChromium\Page;
+
+
+/**
+ *
+ */
+class RemitSpiderCTSLink {
+    const DEFAULT_TIMEZONE = 'America/New_York';
+
+    protected bool   $debug;
+    protected string $pathToScreenshots;
+    protected string $timezone;
+
+    /**
+     * @var CTSLinkBrowser
+     */
+    public CTSLinkBrowser               $CTSLinkBrowser;
+//    public Debug                       $Debug;
+//    public Login                       $Login;
+//    public Portfolios                  $Portfolios;
+//    public Deals                       $Deals;
+//    public HistoryLinks                $HistoryLinks;
+//    public FileIndex                   $FileIndex;
+//    public PrincipalAndInterestFactors $PrincipalAndInterestFactors;
+//    public PeriodicReportsSecured      $PeriodicReportsSecured;
+
+
+
+
+//    protected string $pathToPortfolioIds;
+//    protected string $pathToDealLinkSuffixes;
+//    protected string $pathToHistoryLinks;
+//    protected string $pathToFileIndex;
+//    protected string $timezone;
+//
+//    protected array $portfolioIds;
+//    protected array $dealIds;
+//
+//    protected Page $page;
+
+
+    const  BASE_URL                                = 'https://www.ctslink.com';
+//    const  PORTFOLIO_IDS_FILENAME                  = '_portfolio_ids.json';
+//    const  DEAL_LINK_SUFFIXES_FILENAME             = '_deal_link_suffixes.json';
+//    const  HISTORY_LINKS_FILENAME                  = '_history_links.json';
+//    const  FILE_INDEX_FILENAME                     = '_file_index.json';
+
+
+
+    /**
+     * TESTING, not sure if this will work.
+     *
+     * @var CookiesCollection Saving the cookies post login. When the connection dies for no reason, I can restart the
+     *      session.
+     */
+    public CookiesCollection $cookies;
+
+
+
+    public function __construct( string $chromePath,
+                                 string $user,
+                                 string $pass,
+                                 bool   $debug = FALSE,
+                                 string $pathToScreenshots = '',
+                                 string $pathToPortfolioIds = '',
+                                 string $pathToDealLinkSuffixes = '',
+                                 string $pathToHistoryLinks = '',
+                                 string $pathToFileIndex = '',
+                                 string $pathToFileDownloads = '',
+                                 string $timezone = self::DEFAULT_TIMEZONE
+    ) {
+
+        $this->debug                             = $debug;
+        $this->pathToScreenshots                 = $pathToScreenshots;
+//        $this->pathToPortfolioIds                = $pathToPortfolioIds . self::PORTFOLIO_IDS_FILENAME;
+//        $this->pathToDealLinkSuffixes            = $pathToDealLinkSuffixes . self::DEAL_LINK_SUFFIXES_FILENAME;
+//        $this->pathToHistoryLinks                = $pathToHistoryLinks . self::HISTORY_LINKS_FILENAME;
+//        $this->pathToFileIndex                   = $pathToFileIndex . self::FILE_INDEX_FILENAME;
+
+        $this->timezone = $timezone;
+
+        $this->CTSLinkBrowser = new CTSLinkBrowser( $chromePath );
+        $this->CTSLinkBrowser->page->setDownloadPath( $pathToFileDownloads );
+
+//        $this->Debug = new Debug( $this->USBankBrowser->page,
+//                                  $pathToScreenshots,
+//                                  $debug,
+//                                  $this->timezone );
+//
+//        $this->Login = new Login( $this->USBankBrowser->page,
+//                                  $this->Debug,
+//                                  $user,
+//                                  $pass,
+//                                  $this->timezone );
+
+//        $this->Portfolios = new Portfolios( $this->USBankBrowser->page,
+//                                            $this->Debug,
+//                                            $this->pathToPortfolioIds,
+//                                            $this->timezone );
+//
+//        $this->Deals = new Deals( $this->USBankBrowser->page,
+//                                  $this->Debug,
+//                                  $this->pathToDealLinkSuffixes,
+//                                  $this->timezone );
+//
+//        $this->HistoryLinks = new HistoryLinks( $this->USBankBrowser->page,
+//                                                $this->Debug,
+//                                                $this->pathToHistoryLinks,
+//                                                $this->timezone );
+//
+//        $this->FileIndex = new FileIndex( $this->USBankBrowser->page,
+//                                          $this->Debug,
+//                                          $this->pathToFileIndex,
+//                                          $this->timezone );
+//
+//        $this->PrincipalAndInterestFactors = new PrincipalAndInterestFactors( $this->USBankBrowser->page,
+//                                                                              $this->Debug,
+//                                                                              $this->timezone );
+//        $this->PeriodicReportsSecured      = new PeriodicReportsSecured( $this->USBankBrowser->page,
+//                                                                         $this->Debug,
+//                                                                         $this->timezone );
+    }
+
+
+    /**
+     *
+     */
+//    private function _loadIds() {
+//        if ( file_exists( $this->pathToPortfolioIds ) ):
+//            $this->portfolioIds = file( $this->pathToPortfolioIds );
+//        else:
+//            file_put_contents( $this->pathToPortfolioIds, NULL );
+//        endif;
+//
+//        if ( file_exists( $this->pathToDealLinkSuffixes ) ):
+//            $this->dealIds = file( $this->pathToDealLinkSuffixes );
+//        else:
+//            file_put_contents( $this->pathToDealLinkSuffixes, NULL );
+//        endif;
+//    }
+
+
+}
---
+++ tests/RemitSpiderCTSLinkTest.php
@@ -0,0 +1,108 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use DPRMC\RemitSpiderCTSLink\RemitSpiderCTSLink;
+/**
+ * To run tests call:
+ * php ./vendor/phpunit/phpunit/phpunit --group=first
+ * Class BusinessDateTest
+ */
+class RemitSpiderCTSLinkTest extends TestCase {
+
+    protected static RemitSpiderCTSLink $spider;
+
+    protected static bool $debug = TRUE;
+
+    const TIMEZONE = 'America/New_York';
+
+
+    private function _getNewSpider(){
+//        $this->spider = new USBankSpider\( $this->debug,
+//                                          $storagePath,
+//                                          $this->timezone );
+    }
+
+
+
+    private function _getSpider(): RemitSpiderCTSLink {
+        return new RemitSpiderCTSLink( $_ENV[ 'CHROME_PATH' ],
+                                                              $_ENV[ 'USBANK_USER' ],
+                                                              $_ENV[ 'USBANK_PASS' ],
+                                                              self::$debug,
+                                                              '',
+                                                              '',
+                                                              '',
+                                                              '',
+                                                              '',
+                                                              '/Users/michaeldrennen/Desktop/files',
+                                                              self::TIMEZONE );
+    }
+
+    public static function setUpBeforeClass(): void {
+
+    }
+
+
+    public static function tearDownAfterClass(): void {
+
+    }
+
+
+    /**
+     * @test
+     */
+    public function testConstructor() {
+        $spider = $this->_getSpider();
+        $this->assertInstanceOf( RemitSpiderCTSLink::class,
+                                 $spider );
+    }
+
+
+    /**
+     * @test
+     * @group all
+     */
+//    public function testAll() {
+//        $spider = $this->_getSpider();
+//        $spider->Login->login();
+//        $portfolioIds = $spider->Portfolios->getAll( $spider->Login->csrf );
+//
+//        $dealLinkSuffixesByPortfolioId = [];
+//        foreach ( $portfolioIds as $portfolioId ):
+//            $dealLinkSuffixesByPortfolioId[ $portfolioId ] = $spider->Deals->getAllByPortfolioId( $portfolioId );
+//        endforeach;
+//
+//
+//        $historyLinksByPortfolioId = [];
+//        $dealIdToDealName          = [];
+//        foreach ( $dealLinkSuffixesByPortfolioId as $portfolioId => $dealLinkSuffixes ):
+//            $historyLinksByPortfolioId[$portfolioId] = [];
+//            foreach ( $dealLinkSuffixes as $dealLinkSuffix ):
+//                $historyLinks                         = $spider->HistoryLinks->getAllByDeal( $dealLinkSuffix );
+//                $dealId                               = $spider->HistoryLinks->getDealId();
+//                $dealName                             = $spider->HistoryLinks->getDealName();
+//                $dealIdToDealName[ $dealId ]          = $dealName;
+//                $historyLinksByPortfolioId[$portfolioId][ $dealId ] = $historyLinks;
+//            endforeach;
+//        endforeach;
+//
+//
+//
+//        $fileIndexes = [];
+//        foreach ( $historyLinksByPortfolioId as $portfolioId => $dealIds ):
+//            $fileIndexes[$portfolioId] = [];
+//            foreach ( $dealIds as $dealId => $historyLinks ):
+//                $fileIndexes[$portfolioId][$dealId] = [];
+//                foreach($historyLinks as $historyLinkSuffix):
+//                    $tempFileIndexes = $spider->FileIndex->getAllFromHistoryLink( $historyLinkSuffix);
+//                    $fileIndexes[$portfolioId][$dealId] = array_merge($fileIndexes[$portfolioId][$dealId], $tempFileIndexes);
+//                endforeach;
+//            endforeach;
+//        endforeach;
+//
+//
+//        print_r($fileIndexes);
+//    }
+
+
+}
