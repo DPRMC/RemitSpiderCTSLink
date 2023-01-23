f3541139f4c3aff7ddf4f2193843de2bbe723131
Michael Drennen <michaeldrennen74@gmail.com>
2023-01-23 12:00:11 -0700

    Adding code.

--- src/Helpers/CMBSDistributionFiles.php
+++ src/Helpers/CMBSDistributionFiles.php
@@ -25,6 +25,11 @@ class CMBSDistributionFiles {
     const CMBS_MAIN_PAGE = 'https://www.ctslink.com/a/shelflist.html?shelfType=CMBS';
 
 
+    const type   = 'type';
+    const shelf  = 'shelf';
+    const series = 'series';
+
+    const href                       = 'href'; // The URL of the page with this data.
     const access                     = 'access';
     const current_cycle              = 'current_cycle';
     const next_cycle                 = 'next_cycle';
@@ -32,6 +37,17 @@ class CMBSDistributionFiles {
     const link_to_doc                = 'link_to_doc';
     const link_to_additional_history = 'link_to_additional_history';
     const revised_date               = 'revised_date';
+
+
+
+
+    /**
+     * Sometimes the CTS link site will time out.
+     * A good idea to keep track of the links we were unable to parse, so
+     * we can go back and try again.
+     * @var array
+     */
+    protected array $linksWeWereUnableToPull = [];
 
 
     public function __construct( Page   &$Page,
@@ -42,43 +58,110 @@ class CMBSDistributionFiles {
         $this->timezone = $timezone;
     }
 
+    public function getLinksWeWereUnableToPull(): array {
+        return $this->linksWeWereUnableToPull;
+    }
 
-    public function getAllRecentCMBSDistributionFiles(): array {
-        $distributionFiles = [];
 
-        $this->Debug->_debug( "Getting Series Report Links" );
+
+    public function getAllRecentCMBSDistributionFileData(): array {
+        $distributionFileData = [];
 
         $this->Page->navigate( self::CMBS_MAIN_PAGE )->waitForNavigation();
 
-        $this->Debug->_screenshot( 'cmbs_main_page' );
-        $this->Debug->_debug( "Loaded the CMBS main page. All series links should be here." );
         $cmbsHTML = $this->Page->getHtml();
 
-        $shelfLinks = $this->_getShelfLinksFromHTML( $cmbsHTML );
+        $shelfLinks = $this->getShelfLinksFromHTML( $cmbsHTML );
 
-        //$this->Debug->_debug( "SHELF LINKS - Found this many: " . count( $shelfLinks ) );
+        foreach ( $shelfLinks as $i => $link ):
+            $this->Debug->_debug( " " . $i . " of " . count($shelfLinks) . " " . $link );
+            try {
+                $this->Debug->_debug( " SHELF LINK: " . $link );
 
+                $this->Page->navigate( $link )->waitForNavigation();
+                $html = $this->Page->getHtml();
 
-        foreach ( $shelfLinks as $link ):
-            //$this->Debug->_debug( " SHELF LINK: " . $link );
-
-            $this->Page->navigate( $link )->waitForNavigation();
-            $html = $this->Page->getHtml();
-
-            // If this link leads to a SERIES page, then we can start looking for
-            // a link to a recent Distribution File page.
-            if ( $this->_isSeriesPage( $html ) ):
-                //$this->Debug->_debug( "IS SERIES PAGE: " . $link );
-                $distributionFiles[] = $this->_getDistributionFile( $html );
-            else:
-                //$this->Debug->_debug( "IS SHELF PAGE: " . $link );
-            endif;
-
+                // If this link leads to a SERIES page, then we can start looking for
+                // a link to a recent Distribution File page.
+                if ( $this->_isSeriesPage( $html ) ):
+                    $distributionFileData[] = $this->_getDistributionDateStatementData( $html, $link );
+                else:
+                    $seriesLinks = $this->_getSeriesLinksFromShelfPageHtml( $html );
+                    foreach ( $seriesLinks as $seriesLink ):
+                        $this->Page->navigate( $seriesLink )->waitForNavigation();
+                        $html = $this->Page->getHtml();
+                        if ( $this->_isSeriesPage( $html ) ):
+                            $distributionFileData[] = $this->_getDistributionDateStatementData( $html, $seriesLink );
+                        else:
+                            $this->Debug->_debug( "WTF is: " . $seriesLink );
+                        endif;
+                    endforeach;
+                endif;
+            } catch (\HeadlessChromium\Exception\OperationTimedOut $exception) {
+                $this->Debug->_debug($exception->getMessage());
+                $this->Debug->_debug($link);
+                $this->linksWeWereUnableToPull[] = $link;
+            } catch (\Exception $exception) {
+                $this->Debug->_debug($exception->getMessage());
+            }
         endforeach;
+
+
+        $filesWeHaveAccessTo = [];
+        foreach($distributionFileData as $fileData):
+            if($fileData[self::access]):
+                $filesWeHaveAccessTo[] = $fileData;
+            endif;
+        endforeach;
+
+        return $filesWeHaveAccessTo;
+    }
+
+
+    public function getAllRecentCMBSDistributionFiles(): array {
+        $distributionFiles = [];
+        $filesWeHaveAccessTo = $this->getAllRecentCMBSDistributionFileData();
+
+        // TODO add code to download PDFs.
 
         return $distributionFiles;
     }
 
+
+    /**
+     * If there are multiple SERIES under a SHELF, then I need to parse out the links
+     * to the SERIES pages from this shelf HTML.
+     * @param string $html
+     * @return array
+     */
+    protected function _getSeriesLinksFromShelfPageHtml( string $html ): array {
+        $seriesLinks = [];
+
+        $dom = new \DOMDocument();
+        @$dom->loadHTML( $html );
+
+        /**
+         * @var \DOMNodeList $elements
+         */
+        $elements = $dom->getElementsByTagName( 'a' );
+
+        /**
+         *
+         */
+        foreach ( $elements as $element ):
+            $seriesLink = trim( $element->getAttribute( 'href' ) );
+            if ( empty( $seriesLink ) ):
+                continue;
+            endif;
+
+            if ( FALSE === str_contains( $seriesLink, '/a/seriesdocs.html?shelfId=' ) ):
+                continue;
+            endif;
+            $seriesLinks[] = self::BASE_URL . $seriesLink;
+        endforeach;
+
+        return $seriesLinks;
+    }
 
     /**
      * If this page is a series page, then the string "Restricted Report"
@@ -100,7 +183,7 @@ class CMBSDistributionFiles {
      * @param string $html
      * @return array Ex: https://www.ctslink.com/a/serieslist.html?shelfId=ZHD
      */
-    protected function _getShelfLinksFromHTML( string $html ): array {
+    public function getShelfLinksFromHTML( string $html ): array {
         $shelfLinks = [];
 
         $dom = new \DOMDocument();
@@ -129,7 +212,7 @@ class CMBSDistributionFiles {
      * @param string $href
      * @return bool
      */
-    private function _isShelfLinkOnCMBSPage( string $href ): bool {
+    protected function _isShelfLinkOnCMBSPage( string $href ): bool {
         $pattern = '/a\/serieslist\.html\?shelfId=/';
         $found   = preg_match( $pattern, $href, $matches );
         if ( 1 !== $found ):
@@ -139,11 +222,11 @@ class CMBSDistributionFiles {
     }
 
 
-    protected function _getDistributionFile( string $html ) {
+    protected function _getDistributionFile( string $html, string $href ) {
         $distributionFile = '';
         //$this->Debug->_debug( "Getting shelf links from a SERIES PAGE." );
 
-        $distributionFileData = $this->_getDistributionDateStatementData( $html );
+        //$distributionFileData = $this->_getDistributionDateStatementData( $html, $href );
 
         //print_r( $distributionFileData );
 
@@ -161,7 +244,11 @@ class CMBSDistributionFiles {
     }
 
 
-    private function _getDistributionDateStatementData( string $html ): array {
+    private function _getDistributionDateStatementData( string $html, string $href ): array {
+//        $this->Debug->_html( md5( $html ) );
+//        $this->Debug->_screenshot( md5( $html ) );
+        $seriesData = $this->_getSecurityNamePartsFromHtml( $html, $href );
+
         $data = [];
         $dom  = new \DOMDocument();
         @$dom->loadHTML( $html );
@@ -177,29 +264,73 @@ class CMBSDistributionFiles {
          */
         foreach ( $trs as $tr ):
             if ( $this->_rowContainsDistributionFile( $tr ) ):
-                $rowData = $this->__getDistributionDateStatementRowData( $tr );
-
-                print_r( $rowData );
+                $rowData                 = $this->__getDistributionDateStatementRowData( $tr, $href );
+                $rowData[ self::type ]   = $seriesData[ self::type ];
+                $rowData[ self::shelf ]  = $seriesData[ self::shelf ];
+                $rowData[ self::series ] = $seriesData[ self::series ];
                 $data[] = $rowData;
             endif;
         endforeach;
-
 
         return $data;
     }
 
 
     /**
-     * There is a bunch of "meta data" stored in the HTML row of the table.
+     * @param string $html
+     * @param string $href
+     * @return array
+     * @throws \Exception
+     */
+    private function _getSecurityNamePartsFromHtml( string $html, string $href ): array {
+        $dom  = new \DOMDocument();
+        @$dom->loadHTML( $html );
+        $xpath     = new \DOMXPath( $dom );
+        $classname = 'c67';
+        $nodes     = $xpath->query( "//*[contains(@class, '$classname')]" );
+        $ul        = $nodes->item( 0 );
+        $lis       = $ul->childNodes;
+
+        $bulletItems = [];
+        /**
+         * @var \DOMNode $li
+         */
+        foreach ( $lis as $li ):
+            $bulletItems[] = trim( $li->textContent );
+        endforeach;
+
+        // Remove empty elements
+        $bulletItems = array_filter( $bulletItems );
+
+        // Remove "Home" from the rest
+        array_shift( $bulletItems );
+
+        if ( 3 != count( $bulletItems ) ):
+            throw new \Exception( "Not the right number of security name parts in . " . $href );
+        endif;
+
+        $data                 = [];
+        $data[ self::type ]   = $bulletItems[ 0 ];
+        $data[ self::shelf ]  = $bulletItems[ 1 ];
+        $data[ self::series ] = $bulletItems[ 2 ];
+
+        return $data;
+    }
+
+
+    /**
+     * There is a bunch of "metadata" stored in the HTML row of the table.
      * May as well rip it out and return it.
      * @param \DOMElement $tr
+     * @param string $href
      * @return array
      */
-    private function __getDistributionDateStatementRowData( \DOMElement $tr ): array {
+    private function __getDistributionDateStatementRowData( \DOMElement $tr, string $href ): array {
         $data  = [];
         $tds   = $tr->getElementsByTagName( 'td' );
         $links = $tr->getElementsByTagName( 'a' );
 
+        $data[ self::href ]           = $href;
         $data[ self::access ]         = $this->__weHaveAccessToDistributionFile( $tds->item( 1 )->textContent );
         $data[ self::current_cycle ]  = $this->__getCurrentCycleDate( $tds->item( 2 )->textContent );
         $data[ self::next_cycle ]     = trim( $tds->item( 3 )->textContent );
@@ -220,12 +351,10 @@ class CMBSDistributionFiles {
     }
 
 
-
-    private function __getCurrentCycleDate(string $textContent): string {
+    private function __getCurrentCycleDate( string $textContent ): string {
         $parts = explode( ' ', trim( $textContent ) );
-        return trim($parts[0]);
+        return trim( $parts[ 0 ] );
     }
-
 
 
     private function __getRevisedDate( string $textContent ): string {
