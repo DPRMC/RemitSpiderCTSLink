19bb569d22d9e0a4b951b8a7e4a7fcbeaf3b7a8f
Michael Drennen <michaeldrennen74@gmail.com>
2023-03-31 17:06:19 -0600

    Added a new header to the Advance Recovery factory.

--- src/Factories/CMBSRestrictedServicerReport/AdvanceRecoveryFactory.php
+++ src/Factories/CMBSRestrictedServicerReport/AdvanceRecoveryFactory.php
@@ -3,7 +3,8 @@
 namespace DPRMC\RemitSpiderCTSLink\Factories\CMBSRestrictedServicerReport;
 
 class AdvanceRecoveryFactory extends AbstractTabFactory {
-    protected array $firstColumnValidTextValues = [ 'Tran ID', 'Trans ID' ];
+    protected array $firstColumnValidTextValues = [ 'Tran ID', 'Trans ID', 'Trans
+ID' ];
 
 
 
