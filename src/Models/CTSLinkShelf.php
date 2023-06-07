e52d97bb67b6a81ca5f10a95d12b7246af88cbae
Michael Drennen <michaeldrennen74@gmail.com>
2023-06-07 09:08:23 -0600

    Created a generic helper.

--- src/Models/CTSLinkShelf.php
+++ src/Models/CTSLinkShelf.php
@@ -3,18 +3,28 @@
 namespace DPRMC\RemitSpiderCTSLink\Models;
 
 
-
-
 class CTSLinkShelf {
 
     public string $productType = '';
-    public string $issuerName = '';
-    public string $href = '';
+    public string $issuerName  = '';
+    public string $href        = '';
+    public string $shelf       = '';
 
-    public function __construct(string $productType, string $issuerName, string $href) {
+    public function __construct( string $productType, string $issuerName, string $href ) {
         $this->productType = $productType;
-        $this->issuerName = $issuerName;
-        $this->href = $href;
+        $this->issuerName  = $issuerName;
+        $this->href        = $href;
+        $this->shelf       = $this->_getShelfFromHref( $href );
+    }
+
+
+    /**
+     * @param string $href /a/serieslist.html?shelfId=WFCM
+     * @return string WFCM
+     */
+    protected function _getShelfFromHref( string $href ): string {
+        $parts = explode( '=', $href );
+        return end( $parts );
     }
 
 }
