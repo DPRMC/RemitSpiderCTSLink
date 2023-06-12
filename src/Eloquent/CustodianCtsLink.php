e82bf10d1ad52ebf3105dd3a250a0886a096ef5e
Michael Drennen <michaeldrennen74@gmail.com>
2023-06-12 13:46:49 -0600

    Created a generic helper that returns Document link models.

---
+++ src/Eloquent/CustodianCtsLink.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace DPRMC\RemitSpiderCTSLink\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ *
+ */
+class CustodianCtsLink extends Model {
+
+    public $table        = 'custodian_cts_links';
+    public $primaryKey   = self::id;
+    public $keyType      = 'integer';
+    public $incrementing = TRUE;
+
+    const id              = 'id';
+    const created_at      = 'created_at';
+    const updated_at      = 'updated_at';
+    const shelf           = 'shelf';
+    const series          = 'series';
+    const url             = 'url';
+    const name            = 'name';
+    const downloaded_at   = 'downloaded_at';
+    const downloaded_name = 'downloaded_name';
+
+    protected $casts = [
+        self::shelf           => 'string',
+        self::series          => 'string',
+        self::url             => 'string',
+        self::name            => 'string',
+        self::downloaded_at   => 'datetime',
+        self::downloaded_name => 'string ',
+    ];
+
+    protected $guarded = [];
+
+
+    public function __construct( array $attributes = [] ) {
+        parent::__construct( $attributes );
+        $this->connection = env( 'DB_CONNECTION_CUSTODIAN_CTS' );
+    }
+
+
+}
