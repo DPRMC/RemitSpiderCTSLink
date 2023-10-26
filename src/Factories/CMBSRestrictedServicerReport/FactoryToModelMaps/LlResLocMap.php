1006b4bbdeba3662b73b7d7f666229f242f5c397
Michael Drennen <michaeldrennen74@gmail.com>
2023-10-26 15:19:50 -0600

    Created the LLRESLOC map.

---
+++ src/Factories/CMBSRestrictedServicerReport/FactoryToModelMaps/LlResLocMap.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace DPRMC\RemitSpiderCTSLink\Factories\CMBSRestrictedServicerReport\FactoryToModelMaps;
+
+use DPRMC\RemitSpiderCTSLink\Eloquent\CustodianCtsCmbsRestrictedServicerReportLlResLoc;
+
+class LlResLocMap extends AbstractFactoryToModelMap {
+
+    public static array $map = [
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::date                             => [ 'date' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::trans_id                         => [ 'trans_id' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::group_id                         => [ 'group_id' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::loan_id                          => [ 'loan_id' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::prospectus_loan_id               => [ 'prospectus_loan_id' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::property_name                    => [ 'property_name' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::paid_through_date                => [ 'paid_through_date' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::current_ending_scheduled_balance => [ 'current_ending_scheduled_balance' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::reserve_account_type             => [ 'reserve_account_type' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::reserve_balance_at_contribution  => [ 'reserve_balance_at_contribution' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::beginning_reserve_balance        => [ 'beginning_reserve_balance' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::reserve_deposits                 => [ 'reserve_deposits' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::reserve_disbursements            => [ 'reserve_disbursements' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::ending_reserve_balance           => [ 'ending_reserve_balance' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::loc_expiration_date              => [ 'loc_expiration_date' ],
+        CustodianCtsCmbsRestrictedServicerReportLlResLoc::comments_loan_level_reserve_loc  => [ 'comments_loan_level_reserve_loc' ],
+    ];
+}
