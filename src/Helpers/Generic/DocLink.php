8acab061e61328e972d0af96f9bee298dc9a3d08
Michael Drennen <michaeldrennen74@gmail.com>
2023-11-16 11:33:06 -0700

    DocLink should be parsing out dates now.

--- src/Helpers/Generic/DocLink.php
+++ src/Helpers/Generic/DocLink.php
@@ -2,6 +2,7 @@
 
 namespace DPRMC\RemitSpiderCTSLink\Helpers\Generic;
 
+use Carbon\Carbon;
 use DPRMC\RemitSpiderCTSLink\Helpers\AbstractHelper;
 use DPRMC\RemitSpiderCTSLink\Helpers\Debug;
 use DPRMC\RemitSpiderCTSLink\Models\CTSLinkShelf;
@@ -11,6 +12,8 @@ use HeadlessChromium\Page;
 
 class DocLink {
 
+    const REGEX_DATE_PATTERN = '/\d{1,2}\/\d{1,2}\/\d{2,4}/';
+    public string $revisedCurrentCycle;
 
     public function __construct( public string $nameOfFile = '',
                                  public string $currentCycle = '',
@@ -20,5 +23,33 @@ class DocLink {
                                  public string $href = '',
                                  public bool   $hasAccess = FALSE ) {
 
+        // Deal with a potentially revised date within the current cycle field.
+        $this->revisedCurrentCycle = '';
+        $lowercaseCurrentCycle     = strtolower( $this->currentCycle );
+        // If the word "revised" appears in the current cycle string, then there are most likely two dates present in the field.
+        // I can use regex to pull them out.
+        if ( str_contains( $lowercaseCurrentCycle, 'revised' ) ):
+            $found = preg_match( self::REGEX_DATE_PATTERN, $lowercaseCurrentCycle, $datesFound );
+            if ( 1 !== $found ):
+                $this->currentCycle        = '';
+                $this->revisedCurrentCycle = '';
+            endif;
+
+            if ( count( $datesFound ) > 2 ):
+                throw new \Exception( "There were more than two dates found in the REVISED current cycle field. I have never seen more than 2 dates there." );
+            endif;
+
+            $collection = collect( [] );
+            foreach ( $datesFound as $stringDate ):
+                $collection->push( Carbon::parse( $stringDate ) );
+            endforeach;
+
+            $sorted = $collection->sortBy( function ( $carbonDate, $key ) {
+                return $carbonDate->timestamp();
+            } );
+
+            $this->currentCycle        = $sorted->shift()->format( 'n/j/Y' );
+            $this->revisedCurrentCycle = $sorted->shift()->format( 'n/j/Y' );
+        endif;
     }
 }
