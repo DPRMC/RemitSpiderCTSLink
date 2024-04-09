1642f9ca9cf2deb32b7da7ffc3e218c100a7637a
Michael Drennen <michaeldrennen74@gmail.com>
2024-04-09 13:29:36 -0500

    Adding dateoffile and document id to constructor.

--- src/Models/CMBSRestrictedServicerReport/CMBSRestrictedServicerReport.php
+++ src/Models/CMBSRestrictedServicerReport/CMBSRestrictedServicerReport.php
@@ -2,6 +2,8 @@
 
 namespace DPRMC\RemitSpiderCTSLink\Models\CMBSRestrictedServicerReport;
 
+use Carbon\Carbon;
+
 class CMBSRestrictedServicerReport {
 
 
@@ -16,20 +18,25 @@ class CMBSRestrictedServicerReport {
     const totalLoan       = 'totalLoan';
     const advanceRecovery = 'advanceRecovery';
 
+    const dateOfFile = 'dateOfFile';
+    const documentId = 'documentId';
 
     public function __construct(
 
-        public readonly array $watchlist,
-        public readonly array $dlsr,
-        public readonly array $reosr,
-        public readonly array $hlmfclr,
-        public readonly array $csfr,
-        public readonly array $llResLOC,
-        public readonly array $totalLoan,
-        public readonly array $advanceRecovery,
-        public readonly array $cleanHeadersByProperty,
-        public readonly array $alerts,
-        public readonly array $exceptions ) {
+        public readonly array   $watchlist,
+        public readonly array   $dlsr,
+        public readonly array   $reosr,
+        public readonly array   $hlmfclr,
+        public readonly array   $csfr,
+        public readonly array   $llResLOC,
+        public readonly array   $totalLoan,
+        public readonly array   $advanceRecovery,
+        public readonly array   $cleanHeadersByProperty,
+        public readonly array   $alerts,
+        public readonly array   $exceptions,
+        public readonly ?Carbon $dateOfFile = NULL,
+        public readonly ?int    $documentId = NULL
+    ) {
     }
 
 }
