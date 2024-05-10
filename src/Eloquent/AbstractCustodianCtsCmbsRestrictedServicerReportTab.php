debc7fb82b29248c078c568a13806868051bfd25
Michael Drennen <michaeldrennen74@gmail.com>
2024-05-10 10:56:32 -0600

    Created new constants in the AbstractCustodianCtsCmbsRestrictedServicerReportTab

--- src/Eloquent/AbstractCustodianCtsCmbsRestrictedServicerReportTab.php
+++ src/Eloquent/AbstractCustodianCtsCmbsRestrictedServicerReportTab.php
@@ -15,4 +15,11 @@ class AbstractCustodianCtsCmbsRestrictedServicerReportTab extends Model {
     const date        = 'date';
     const document_id = 'document_id';
 
+
+    const prospectus_loan_id = 'prospectus_loan_id';
+    const property_id        = 'property_id';
+    const property_name      = 'property_name';
+    const property_city      = 'property_city';
+    const property_state     = 'property_state';
+
 }
