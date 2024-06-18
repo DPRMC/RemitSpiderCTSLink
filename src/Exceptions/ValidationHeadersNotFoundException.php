ba007fe8f6c46d001cf20e564d5583651027c4a1
Tony Santora <tsantora@deerparkrd.com>
2024-06-18 13:42:01 -0600

    Initial commit

---
+++ src/Exceptions/ValidationHeadersNotFoundException.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace DPRMC\RemitSpiderCTSLink\Exceptions;
+
+use Exception;
+use Throwable;
+
+/**
+ * Throw this exception if a header used to validate the row is missing
+ */
+class ValidationHeadersNotFoundException extends Exception {
+
+    /**
+     * An array containing the missing headers needed for row validation
+     * @var array
+     */
+    public array  $missingValidationHeaders = [];
+
+
+    /**
+     * @param array $missingValidationHeaders
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct( array $missingValidationHeaders, int $code = 0, ?Throwable $previous = NULL ) {
+        $this->missingValidationHeaders  = $missingValidationHeaders;
+        parent::__construct( $this->generateMessage(), $code, $previous );
+    }
+
+
+    /**
+     * Generates the error message with the documentId and duplicate headers
+     * @return string
+     */
+    public function generateMessage() : string {
+        $message = "Header row is missing the following keys needed for validation: ";
+        foreach( $this->missingValidationHeaders as $missingValidationHeader ) :
+            $message .= "'$missingValidationHeader', ";
+        endforeach;
+
+        return rtrim( $message, ", " );
+    }
+}
