2f24cca825bc42d85199e95ef06817d3ab6050ec
Michael Drennen <michaeldrennen74@gmail.com>
2024-07-09 16:27:40 -0600

    Updated with new TransID

--- src/Factories/CMBSRestrictedServicerReport/LLResLOCFactory.php
+++ src/Factories/CMBSRestrictedServicerReport/LLResLOCFactory.php
@@ -3,7 +3,7 @@
 namespace DPRMC\RemitSpiderCTSLink\Factories\CMBSRestrictedServicerReport;
 
 class LLResLOCFactory extends AbstractTabFactory {
-    protected array $firstColumnValidTextValues = [ 'Trans ID', 'Trans', 'Transaction ID' ];
+    protected array $firstColumnValidTextValues = [ 'Trans ID', 'Trans', 'Transaction ID', "Trans\nID" ];
 
     protected function _removeInvalidRows( array $rows = [] ): array {
         return $rows;
