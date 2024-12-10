3f0b85d63031dee057c66cd835c2adb0238f8cb0
Michael Drennen <michaeldrennen74@gmail.com>
2024-12-09 18:55:38 -0700

    Updated Watchlist Factory parsing.

--- src/Factories/CMBSRestrictedServicerReport/WatchlistFactory.php
+++ src/Factories/CMBSRestrictedServicerReport/WatchlistFactory.php
@@ -13,6 +13,143 @@ class WatchlistFactory extends AbstractTabFactory {
                                                     'Tran ID',
                                                     'transaction_id' ];
 
+
+    /**
+     * @param array       $allRows
+     * @param string|NULL $sheetName
+     * @param array       $existingRows
+     *
+     * @return void
+     * @throws \DPRMC\RemitSpiderCTSLink\Exceptions\NoDataInTabException
+     */
+    protected function _setParsedRows( array $allRows, string $sheetName = NULL, array $existingRows = [] ): void {
+        $this->cleanRows = $existingRows;
+
+        $validRows = $this->_getRowsToBeParsed( $allRows );
+
+        foreach ( $validRows as $i => $validRow ):
+            $newCleanRow = [];
+
+            foreach ( $this->localHeaders as $j => $header ):
+                $data                   = trim( $validRow[ $j ] ?? '' );
+                $newCleanRow[ $header ] = $data;
+            endforeach;
+
+            // Some tabs leave the date off.
+            // So set a placeholder of NULL for now, and I will "borrow" the date from another tab.
+            if ( $this->date ):
+                $newCleanRow[ 'date' ] = $this->date->toDateString();
+            else:
+                $newCleanRow[ 'date' ] = NULL;
+            endif;
+
+            $newCleanRow[ 'document_id' ] = $this->documentId;
+
+            // KLUDGE
+            // I have empty rows coming in, and I dont want to bother finding out why.
+            // Only the date is showing up since I added it above.
+            if ( sizeof( $newCleanRow ) == 1 ):
+                continue;
+            endif;
+
+            $this->cleanRows[] = $newCleanRow;
+        endforeach;
+
+
+        // REMOVE THE TOTAL ROW
+        foreach ( $this->cleanRows as $k => $cleanRow ):
+            if (
+                isset( $cleanRow[ 'trans_id' ] ) &&
+                str_contains( strtolower( $cleanRow[ 'trans_id' ] ), 'total' )
+            ):
+                unset( $this->cleanRows[ $k ] );
+            endif;
+        endforeach;
+
+        $this->cleanRows = $this->_removeInvalidRows( $this->cleanRows );
+
+        $this->cleanRows = array_values( $this->cleanRows ); // Reindex the array so the indexes are sequential again.
+    }
+
+
+    /**
+     * @param array $allRows
+     *
+     * @return array
+     * @throws \DPRMC\RemitSpiderCTSLink\Exceptions\NoDataInTabException
+     */
+    protected function _getRowsToBeParsed( array $allRows ): array {
+        $firstBlankRowIndex = 0;
+
+        $firstRowOfDataIndex = $this->_getFirstRowOfDataIfItExists( $allRows );
+        $totalNumRows        = count( $allRows );
+
+        $possibleDataRows = $this->_removeJunkRowsBetweenHeaderAndData( $firstRowOfDataIndex, $allRows );
+
+        foreach ( $possibleDataRows as $i => $possibleDataRow ):
+            if ( empty( $possibleDataRow[ 0 ] ) ):
+                $firstBlankRowIndex = $i;
+                break;
+            endif;
+        endforeach;
+
+        $numRows = count( $possibleDataRows ) - $firstBlankRowIndex;
+
+        $slicedRows = array_slice( $possibleDataRows, 0, $firstBlankRowIndex );
+
+        return $slicedRows;
+    }
+
+
+    /**
+     * @param int   $firstRowOfDataIndex
+     * @param array $allRows
+     *
+     * @return array Possible data rows with junk rows that existed between the header row and the first valid row of data have been removed.
+     */
+    protected function _removeJunkRowsBetweenHeaderAndData( int $firstRowOfDataIndex, array $allRows ): array {
+        $filteredRows     = [];
+        $totalNumRows     = count( $allRows );
+        $length           = $totalNumRows - $firstRowOfDataIndex;
+        $possibleDataRows = array_slice( $allRows, $firstRowOfDataIndex, $length );
+        $possibleDataRows = array_values( $possibleDataRows );
+
+
+        foreach ( $possibleDataRows as $possibleDataRow ):
+            if ( $this->_rowContainsDisqualifyingString( $possibleDataRow ) ):
+                continue;
+            endif;
+            $filteredRows[] = $possibleDataRow;
+        endforeach;
+
+        return $filteredRows;
+    }
+
+
+    /**
+     * There are patterns. There are certain strings that exist in a row, that guarantee
+     * that the row is an invalid row.
+     *
+     * @param array $row
+     *
+     * @return bool
+     */
+    protected function _rowContainsDisqualifyingString( array $row ): bool {
+        $disqualifiedStrings = [
+            'CLTranID',
+            'so long comments will display correctly',
+        ];
+        foreach ( $row as $i => $value ):
+            foreach ( $disqualifiedStrings as $disqualifiedString ):
+                if ( str_contains( $value, $disqualifiedString ) ):
+                    return TRUE;
+                endif;
+            endforeach;
+        endforeach;
+        return FALSE;
+    }
+
+
     protected function _removeInvalidRows( array $rows = [] ): array {
 
         //dd($this->localHeaders);
@@ -26,6 +163,12 @@ class WatchlistFactory extends AbstractTabFactory {
         foreach ( $rows as $i => $row ):
 
             $row = array_slice( $row, 0, count( $this->localHeaders ) );
+
+            // 2024-12-09:mdd @see Doc ID: 6047667
+            if ( 'CLTranID' == $row[ 'trans_id' ] ):
+                dump( 'BAAAAAAD trans_id' );
+                continue;
+            endif;
 
             $numNullCells           = 0;
             $nonIntegerFound        = FALSE;
